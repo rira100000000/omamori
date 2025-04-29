290b38cbcb88ce5b7f26768bec191eb1d157b033
rira100000000 <101010hayakawa@gmail.com>
2025-04-29 11:51:16 +0900

    テストファイル削除

--- test_file.rb
+++
@@ -1,8 +0,0 @@
-# This is a test file for omamori scan
-
-def vulnerable_method(input)
-  eval(input) # Potential code execution vulnerability
-end
-
-user_input = gets.chomp
-vulnerable_method(user_input)
